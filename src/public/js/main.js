9adfde84e10058ec96328838d601a541aac1b239
santiagoanaya <anayasantiagohernan@gmail.com>
2018-12-04 13:55:02 -0300

    Mongodb implementation

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,23 +1,25 @@
 $(function () {
 
-  const socket = io();
+  // socket.io client side connection
+  const socket = io.connect()
 
-  // obtaining DOM elements from the interface
+  // obtaining DOM elements from the Chat Interface
   const $messageForm = $('#message-form')
   const $messageBox = $('#message')
   const $chat = $('#chat')
 
-  // obtaining DOM elements from the nicknameForm
+  // obtaining DOM elements from the NicknameForm Interface
   const $nickForm = $('#nickForm')
   const $nickError = $('#nickError')
   const $nickname = $('#nickname')
 
+  // obtaining the usernames container DOM
   const $users = $('#usernames')
 
   $nickForm.submit(e => {
     e.preventDefault()
     socket.emit('new user', $nickname.val(), data => {
-      if (data) {
+      if(data) {
         $('#nickWrap').hide()
         $('#contentWrap').show()
       } else {
@@ -32,7 +34,7 @@ $(function () {
   })
 
   // events
-  $messageForm.submit(e => {
+  $messageForm.submit( e => {
     e.preventDefault()
     socket.emit('send message', $messageBox.val(), data => {
       $chat.append(`<p class="error">${data}</p>`)
@@ -41,12 +43,12 @@ $(function () {
   })
 
   socket.on('new message', data => {
-    displayMsg(data);
+    displayMsg(data)
   })
 
   socket.on('usernames', data => {
     let html = ''
-    for (let i = 0; i < data.length; i++) {
+    for(i = 0; i < data.length; i++) {
       html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`
     }
     $users.html(html)
@@ -57,7 +59,7 @@ $(function () {
   })
 
   socket.on('load old msgs', msgs => {
-    for(let i = msgs.length -1; i >=0 ; i--) {
+    for(let i = msgs.length -1; i >=0;  i--) {
       displayMsg(msgs[i])
     }
   })
@@ -65,4 +67,5 @@ $(function () {
   function displayMsg(data) {
     $chat.append(`<p class="msg"><b>${data.nick}</b>: ${data.msg}</p>`)
   }
+
 })
